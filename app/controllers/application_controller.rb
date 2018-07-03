3f028dae70e6abb3c33246a2e89a8faa1e800fca
Marilou Serrano <mayuserrano18@gmail.com>
2018-07-03 15:37:41 +0800

    Finish Toy App

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: "hello, world!"
+    render html: "hello, world! I've created my first Ruby on Rails application."
   end
 end
